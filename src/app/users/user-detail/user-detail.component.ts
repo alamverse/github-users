840fadb0d6a8beb1a2a3ea57de7fecd8b3227745
Shah Alam <shahalam9183@gmail.com>
2020-06-15 12:59:01 +0530

    phase 1 completed

--- src/app/users/user-detail/user-detail.component.ts
+++ src/app/users/user-detail/user-detail.component.ts
@@ -6,27 +6,27 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css']
+  styleUrls: ['./user-detail.component.css'],
 })
 export class UserDetailComponent implements OnInit, OnDestroy {
   currentUser;
   currentUserFullData;
   userSub: Subscription;
-  dpStyle: string;
-  constructor(private http: HttpClient, private usersService: UsersService) { }
+  icon: string;
+  constructor(private http: HttpClient, private usersService: UsersService) {}
 
   ngOnInit(): void {
     this.currentUser = this.usersService.getSelectedUser();
-    this.userSub = this.usersService.userSelected.subscribe(user => {
+    this.userSub = this.usersService.userSelected.subscribe((user) => {
       this.currentUser = user;
       this.loadCurrentUserFullData(this.currentUser.url);
     });
   }
 
   loadCurrentUserFullData(url: string) {
-    this.http.get<any>(url).subscribe(d => {
+    this.http.get<any>(url).subscribe((d) => {
       this.currentUserFullData = d;
-      this.dpStyle = 'background-image: ' + this.currentUserFullData.avatar_url + ';';
+      this.icon = `url('${this.currentUserFullData.avatar_url}')`;
     });
   }
 
